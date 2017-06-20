a7d2ece3a3376c7d7005494bcc436d3236a0f08e
Artur Bergen <ab176@hdm-stuttgart.de>
2017-06-20 17:40:24 +0200

    Add i18n translation.

--- middleware/config/db.config.js
+++ middleware/config/db.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-    database: 'mongodb://backend/cncshop',
+    database: 'mongodb://localhost/cncshop',
+//    database: 'mongodb://backend/cncshop',
     secret: '7841e0acd47f4dc03e87d8236e89274956471a56'
 }
