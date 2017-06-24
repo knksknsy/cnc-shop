61d15bb3c3df3d42b93150384f6fbf46e7a69336
Artur Bergen <ab176@hdm-stuttgart.de>
2017-06-24 02:03:42 +0200

    Change crypto to bcrypt in middleware.

--- middleware/models/users.model.js
+++ middleware/models/users.model.js
@@ -2,15 +2,19 @@
 *  Copyright (C) 2017
 *
 *   Kaan K.
+*   Artur B.
 *
 *  MIT License
 */
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var crypto = require('crypto');
-var jwt = require('jsonwebtoken');
+// var crypto = require('crypto');
+//var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs'); // TODO: for a better performance we should use basic bcrypt
 const secret = require('../config/db.config').secret;
+
+var CRYPT_FACTOR = 11;
 
 var userSchema = new Schema({
     email: { type: String, unique: true, required: true },
@@ -20,19 +24,45 @@ var userSchema = new Schema({
     postcode: { type: String, required: true },
     city: { type: String, required: true },
     state: { type: String, required: true },
-    orderId: { type: String, required: false },
-    hash: String,
-    salt: String
+    orderId: { type: String, required: false }
+    // hash: String, //this should not be safed in userSchema
+    // salt: String
 });
 
-userSchema.methods.setPassword = function (password) {
-    this.salt = crypto.randomBytes(16).toString('hex');
-    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
-}
+// Generate hashed, salted and bcrypted passwords
+userSchema.pre('save', function(next) {
+    var user = this;
+    if (!user.isModified('password')) {
+      bcrypt.genSalt(CRYPT_FACTOR, function(err, salt) {
+          if (err) return next(err);
+          bcrypt.hash(user.password, salt, function(err, hash) {
+              if (err) return next(err);
+              user.password = hash;
+              next();
+          });
+      });
+    } else {
+        return next();
+    }
+});
 
-userSchema.methods.validPassword = function (password) {
-    var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
-    return this.hash === hash;
-}
+userSchema.methods.comparePassword = function(password, callback) {
+    bcrypt.compare(password, this.password, function(err, isMatch) {
+        if (err) return callback(err);
+        callback(null, isMatch);
+    });
+};
 
-mongoose.model('Users', userSchema);
+module.exports = mongoose.model('Users', userSchema);
+
+// i think this should be also fine
+// userSchema.methods.setPassword = function (password) {
+//     this.salt = crypto.randomBytes(16).toString('hex');
+//     this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+// }
+//
+// userSchema.methods.validPassword = function (password) {
+//     var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+//     return this.hash === hash;
+// }
+//mongoose.model('Users', userSchema);
