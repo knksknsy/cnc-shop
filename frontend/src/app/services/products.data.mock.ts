d8dbe0b083e4f92d57729b535443573aa129635b
Eli Kabasele <ek058@hdm-stuttgart.de>
2017-06-23 18:15:25 -0700

    Add Mock data for Uni testing

---
+++ frontend/src/app/services/products.data.mock.ts
@@ -0,0 +1,85 @@
+/**
+*  Copyright (C) 2017
+*
+*  Eli K.
+*
+*  MIT License
+*/
+
+
+export const MockProducts = [
+
+       {
+            "name": "CAP'N CAN Artist Acrylic Spray 400 ml",
+            "image": "/assets/products/cans/cnc_can.png",
+            "description": "Mit der CAP'N CAN Artist Acrylic bekommt man nicht nur ein reines, schnelltrocknendes Acrylat, sondern auch eine variable Ventileinstellung mit nur einem Cap. Dadurch lassen sich unterschiedlich feine oder breite Striche ziehen. Die streichfähige Farbe lässt sich außerdem mit einem Pinsel oder einem Spachtel auftragen und besitzt eine hohe UV-Beständigkeit.",
+            "information": [
+                "Acrylbasis (100% reines Acrylat)",
+                "hochdeckend",
+                "permanent",
+                "hohe UV-Beständigkeit",
+                "extrem vergilbungsarm",
+                "streichfähig und spachtelbar",
+                "variable Aussprührate",
+                "Strichbreiten: 4 – 200 mm mit einem Sprühkopf",
+                "400 ml"
+            ],
+            "quantity": 1,
+            "price": 7.95,
+            "category": "cans"
+        },
+        {
+            "name": "CAP'N CAN Artist Neon Spray 400 ml",
+            "image": "/assets/products/cans/cnc_can.png",
+            "description": "Die Neonfarben der CAP'N CAN Special Series knallen so richtig. Die fluoreszierende, hochpigmentierte Farbe deckt auf fast allen Untergründen und eignet sich somit für diverse Kunstanwendungen. In Kombination mit dem CAP'N CAN UV-Varnish lässt sie sich außerdem versiegeln und die eingeschränkte UV-Beständigkeit verbessern.",
+            "information": [
+                "Acrylbasis",
+                "fluoreszierende, hochpigmentierte Neonfarben",
+                "matt",
+                "hochdeckend",
+                "streichfähig und spachtelbar",
+                "variable Aussprührate",
+                "Strichbreiten: 4 – 200 mm mit einem Sprühkopf",
+                "Füllmenge: 400 ml"
+            ],
+            "quantity": 1,
+            "price": 9.95,
+            "category": "cans"
+        },
+        {
+            "name": "CAP'N CAN Artist Chalk Spray 400 ml",
+            "image": "/assets/products/cans/cnc_can.png",
+            "description": "Das hochpigmentierte Kreidespray ist ideal für temporäre Anwendungen auf lackierbaren Untergründen. Zur Entfernung wird die Farbe einfach mit Wasser abgewischt. Für einen permanenten Halt kann die Farbe außerdem mit dem CAP'N CAN UV-Varnish versiegelt werden.",
+            "information": [
+                "hochpigmentierte Kreide-Pigmentfarbe",
+                "matt",
+                "hochdeckend",
+                "abwischbar bei nicht saugfähigen Untergründen",
+                "gute UV-Beständigkeit",
+                "schnelltrocknend",
+                "streichfähig und spachtelbar",
+                "Füllmenge: 400 ml"
+            ],
+            "quantity": 1,
+            "price": 9.95,
+            "category": "cans"
+        },
+        {
+            "name": "CAP'N CAN Artist Effect Spray 400 ml",
+            "image": "/assets/products/cans/cnc_can.png",
+            "description": "Ob Dekorationen oder Kunstanwendungen auf der Leinwand: dieser hochglänzende Effektlack besitzt eine hohe Deckkraft und ist schnelltrocknend. Er eignet sich speziell für Dekorationen im Innenbereich – eine Lackversiegelung ist dabei nicht notwendig.",
+            "information": [
+                "hochpigmentierter Dekorationslack",
+                "hochglänzend",
+                "hochdeckend",
+                "nicht abriebfest, bedingt permanent",
+                "schnelltrocknend",
+                "für fast alle Untergründe",
+                "Füllmenge: 400 ml"
+            ],
+            "quantity": 1,
+            "price": 9.95,
+            "category": "cans"
+        }
+
+]
