ba44a9eafc879d71ad9e072a0cd8987d962a009e
Artur Bergen <ab176@hdm-stuttgart.de>
2017-06-24 04:18:20 +0200

    Add translation 4 cart modal.

--- middleware/config/db.config.js
+++ middleware/config/db.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     //database: 'mongodb://backend/cncshop',
     database: 'mongodb://localhost/cncshop',
-    userDatabase : 'mongodb://localhost:27017/cncshopUsers',
     cookie: { name: 'mySession', secret: '7841e0acd47f4dc03e87d8236e89274956471a56' }
 }
